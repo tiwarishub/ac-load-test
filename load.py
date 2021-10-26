e73807103d5d86d432115e78520a2e162f6b0408
aparna-ravindra <aparna-ravindra@github.com>
2021-10-26 11:36:35 +0530

    arranging rpm

--- load.py
+++ load.py
@@ -20,34 +20,38 @@ def dispatch_job():
         log_err("Dispatched Job Failed")
         log_err(e)
 
-def dispatch_download_cache(max_time, rpm):
-    start_time = current_milli_time()
-    distance_between_calls_ms = 60000/rpm
+def distpatch_download_cache():
     try:
-        while (current_milli_time() - start_time) < max_time * 1000:
-            c_start_time = current_milli_time()
-            call(["node", "dist/download_cache.js"]) 
-            if distance_between_calls_ms - (current_milli_time() - c_start_time) > 0:
-                print("sleeping for seconds:"+ (distance_between_calls_ms - (current_milli_time() - c_start_time) ) / 1000)
-                time.sleep((distance_between_calls_ms - (current_milli_time() - c_start_time) ) / 1000)
-            
+        call(["node", "dist/download_cache.js"]) 
     except Exception as e:
-        log_err("Dispatched dispatch_download_cache Job Failed")
+        log_err("distpatch_download_cache Failed")
         log_err(e)
+
+def start_download_cache(max_time, rpm):
+    start_time = time.time()
+    interval_s = (1/rpm)*60
+    while (time.time() - start_time) < max_time:
+        if (time.time() - start_time) < max_time:
+                t = Thread(target=distpatch_download_cache)
+                t.start()
+                time.sleep(interval_s)
+        else:
+            print("~Finished stagger load test! *high five* ~")
+            break
 
 start_time = time.time()
 jobs_per_min = 2
 interval_s = (1/jobs_per_min)*60
 max_time = 120 
-download_cache_thread = Thread(target=dispatch_download_cache, args=(max_time, 150))
+download_cache_thread = Thread(target=start_download_cache, args=(max_time, 300))
 download_cache_thread.start()
 
 while (time.time() - start_time) < max_time:
-    for x in range(0, 2):
-        if (time.time() - start_time) < max_time:
+    if (time.time() - start_time) < max_time:
             t = Thread(target=dispatch_job)
             t.start()
             time.sleep(interval_s)
-        else:
-            print("~Finished stagger load test! *high five* ~")
-            break
+    else:
+        print("~Finished stagger load test! *high five* ~")
+        break
+        
