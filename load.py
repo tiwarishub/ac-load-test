24f52adb269bb63a25120e5a1972494824493dc4
Shubham Tiwari <64764738+tiwarishub@users.noreply.github.com>
2021-11-02 13:01:18 +0530

    Update load.py

--- load.py
+++ load.py
@@ -42,12 +42,12 @@ def start_download_cache(max_time, rpm):
 # Get args
 parser = argparse.ArgumentParser()
 parser.add_argument("download_cache_thread_rpm", help="GenerateArtifactDownloadV1 API requests per min", type=str)
-parser.add_argument("jobs_per_min", help="Upload Jobs to run per minute", type=int)
+parser.add_argument("jobs_per_min", help="Upload Jobs to run per minute", type=float)
 parser.add_argument("time_to_run_in_minutes", help="Time to run in minutes", type=int)
 args = parser.parse_args()
 
 start_time = time.time()
-interval_s = (1/args.jobs_per_min)*60
+interval_s = (1.0/args.jobs_per_min)*60
 max_time = 60 * args.time_to_run_in_minutes 
 download_cache_thread_rpm = float(args.download_cache_thread_rpm)
 download_cache_thread = Thread(target=start_download_cache, args=(max_time, download_cache_thread_rpm))
